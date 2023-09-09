50747238a76498e124a5690055a1cd146f34ebf8
Alexander Babic <alexander.t.b@hotmail.com>
2023-09-09 18:24:40 +0800

    added code into tag-route.js, updated model.js to match criteria

--- models/Product.js
+++ models/Product.js
@@ -12,23 +12,28 @@ Product.init(
     // define columns
     id: {
       type: DataTypes.INTEGER,
+      allowNull: false,
       primaryKey: true,
-      autoIncrement: true,
+      autoIncrement: true
     },
     product_name: {
       type: DataTypes.STRING,
       allowNull: false,
-      validate: {
-        isAlphanumeric: true,
-      }
     },
     price: {
       type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
+      validate: {
+        isDecimal: true,
+      }
     },
     stock: {
       type: DataTypes.INTEGER,
+      defaultValue: 10,
       allowNull: false,
+      validate: {
+        isNumeric: true,
+      }
     },
     category_id: {
       type: DataTypes.INTEGER,
--- models/index.js
+++ models/index.js
@@ -4,14 +4,33 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongs To Category
+Product.belongsTo(Category, {
+  foreignkey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignkey: 'category_id',
+  onDelete: 'CASCADE',
+})
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tags'
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'products'
+});
 module.exports = {
   Product,
   Category,
